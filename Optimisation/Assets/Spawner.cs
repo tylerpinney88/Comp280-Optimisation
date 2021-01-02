e4080e3d935e39d8a9a7b188b1b88f752e5ba4e3
tylerpinney88 <56723285+tylerpinney88@users.noreply.github.com>
2021-01-02 20:36:43 +0000

    Done

---
+++ Optimisation/Assets/Spawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject spawner;
+    public GameObject Sphere;
+    public int counter;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnsSphere", 0, (0.1f/counter));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    void SpawnsSphere()
+    {
+        Instantiate(Sphere, transform.position, transform.rotation);
+    }
+}
---
+++ Optimisation/Assets/SphereController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereController : MonoBehaviour
+{
+    public Rigidbody rb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(this.gameObject, 5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
